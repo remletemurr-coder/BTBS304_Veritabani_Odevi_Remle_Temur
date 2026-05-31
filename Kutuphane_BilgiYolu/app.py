ffb5875ed5a334f73b0d79ad6154f6769cca63a4
remletemurr-coder <remletemurr@gmail.com>
2026-05-31 19:19:45 +0300

    değişiklikler yapılıyor

--- Kutuphane_BilgiYolu/app.py
+++ Kutuphane_BilgiYolu/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_parser # Eğer url_parser hata verirse direkt kaldırabilirsin
+from flask import Flask, render_template, request, redirect 
 from flask import url_for 
 from logic import BusinessLayer
 
